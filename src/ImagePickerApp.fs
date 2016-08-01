7862f0c13c92fb0e0e2d2c24ccc0a3b3bb5367d6
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-08-01 12:22:26 +0200

    Add NavigationBar

--- src/ImagePickerApp.fs
+++ src/ImagePickerApp.fs
@@ -18,7 +18,6 @@ type ImagePickerAppState = {
     uri: string
 }
 
-
 let baseUrl = "http://facebook.github.io/react/img/logo_og.png"
 
 type ImagePickerApp (props) as this =
@@ -60,7 +59,50 @@ type ImagePickerApp (props) as this =
                 ]
                 [ ]
 
+        let mainView = 
+            view [ ViewProperties.Style [ViewStyle.Flex 1]] 
+             [ image
+               button ]
 
-        view [ ViewProperties.Style [ViewStyle.Flex 1]] 
-         [ image
-           button ]
+        let routes =
+            let r = createEmpty<Route>
+            r.title <- Some "First Scene"
+            r.index <- Some 0
+
+            let r2 = createEmpty<Route>
+            r2.title <- Some "Second Scene"
+            r2.index <- Some 1
+
+            [|r; r2|]
+
+        let nav =
+           navigator 
+             [NavigatorProperties.InitialRoute routes.[0]
+              NavigatorProperties.InitialRouteStack routes
+              NavigatorProperties.RenderScene
+                (Func<_,_,_>(fun route navigator ->
+                    text [] (sprintf "Hello %s!" route.title) 
+                    |> touchableHighlight [
+                        TouchableHighlightProperties.Style [
+                            ViewStyle.BackgroundColor "#AA00AA"
+                            ViewStyle.Flex 1
+                        ]                    
+                        OnPress
+                            (fun () ->
+                                if route.index = 0 then
+                                    navigator.push(routes.[1])
+                                else
+                                    navigator.pop() 
+                            )])
+                )
+              NavigatorProperties.NavigationBar 
+                (navigationBar [
+                    NavigationBarProperties.Style [ViewStyle.BackgroundColor "#00ff00"]
+                    RouteMapper [
+                        NavigationBarRouteMapperProperties.LeftButton (Func<_,_,_,_,_>(fun route navigator index navState -> text [] "Cancel"))
+                        NavigationBarRouteMapperProperties.RightButton (Func<_,_,_,_,_>(fun route navigator index navState -> text [] "Done"))
+                        NavigationBarRouteMapperProperties.Title (Func<_,_,_,_,_>(fun route navigator index navState -> text [] "Awesome Nav Bar"))
+                    ] 
+                ])
+             ]
+        nav
