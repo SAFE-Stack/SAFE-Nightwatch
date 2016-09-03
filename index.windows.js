f7e69be9d39fe20c13b8aad48ec3b8d48a8dfbf0
DavidPodhola <david.podhola@naseukoly.cz>
2016-09-03 10:52:45 +0200

    make working with https://github.com/ReactWindows/react-native-windows too

---
+++ index.windows.js
@@ -0,0 +1,5 @@
+import * as coreJs from "core-js/shim";
+import {AppRegistry} from 'react-native';
+import {Nightwatch} from './out/Nightwatch';
+
+AppRegistry.registerComponent('Nightwatch', () => Nightwatch);
