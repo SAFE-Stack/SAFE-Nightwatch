e3157b907ab84d9291632ebc97fe4d1a27e66f25
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-11-18 09:42:50 +0100

    Setup back button

--- src/Nightwatch.fs
+++ src/Nightwatch.fs
@@ -9,8 +9,19 @@ open Elmish
 open Elmish.React
 open Elmish.ReactNative
 open Messages
+    
+let setupBackHandler dispatch =
+    let backHandler () =
+        dispatch AppMsg.NavigateBack
+        true
+
+    Fable.Helpers.ReactNative.setOnHardwareBackPressHandler backHandler
+
+let subscribe _ =
+    Cmd.ofSub setupBackHandler
 
 let runnable :obj->obj = 
     Program.mkProgram App.init App.update App.view
+    |> Program.withSubscription subscribe
     |> Program.withConsoleTrace
     |> Program.toRunnable Program.run
