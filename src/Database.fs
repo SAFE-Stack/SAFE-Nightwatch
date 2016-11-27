ff24539cb28aefe99c4c30cde806612612a4ba13
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-11-27 19:32:32 +0100

    Upgrade to Fable 0.7.0

--- src/Database.fs
+++ src/Database.fs
@@ -7,30 +7,30 @@ open Fable.Import.ReactNative
 open Fable.Helpers.ReactNative
 open Fable.Helpers.ReactNative.Props
 open Fable.Helpers.ReactNativeSimpleStore
-open Fable.Import.Fetch
-open Fable.Helpers.Fetch
+open Fable.PowerPack
 open Model
 
 let createDemoData() =
-    async { 
+    async {
         try
             do! DB.clear<LocationCheckRequest>()
-            // Fetch demo data 
+            // Fetch demo data
             let! requests =
-                fetchAs<LocationCheckRequest[]>
+                Fetch.fetchAs<LocationCheckRequest[]>
                     ("https://raw.githubusercontent.com/fsprojects/fable-react_native-demo/master/demodata/LocationCheckRequests.json",
                     [])
+                |> Async.AwaitPromise
             do! DB.addMultiple requests
             return requests.Length
         with
         | error -> return 0
-    } 
+    }
 
 
 let getIndexedCheckRequests () =
     async {
         let! requests = DB.getAll<Model.LocationCheckRequest>()
-        return 
+        return
             requests
             |> Array.mapi (fun i r -> i,r)
     }
