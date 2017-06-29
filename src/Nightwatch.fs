5bb4895f11148724503436328dabf367ce281ea6
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-06-29 17:29:26 +0200

    Put console trace under debug

--- src/Nightwatch.fs
+++ src/Nightwatch.fs
@@ -6,6 +6,8 @@ open Elmish.ReactNative
 
 Program.mkProgram App.init App.update App.view
 |> Program.withSubscription App.subscribe
+#if DEBUG
 |> Program.withConsoleTrace
+#endif
 |> Program.withReactNative "nightwatch"
 |> Program.run
