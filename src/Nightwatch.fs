c721a15d00838fbc1e56664accb6a272d5b374a9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-06-30 18:14:43 +0200

    add debugger

--- src/Nightwatch.fs
+++ src/Nightwatch.fs
@@ -2,6 +2,7 @@ module Nightwatch
 
 open Elmish
 open Elmish.React
+open Elmish.Debug
 open Elmish.ReactNative
 open Elmish.HMR
 
@@ -23,6 +24,7 @@ Program.mkProgram App.init App.update App.view
 #if RELEASE
 #else
 |> Program.withConsoleTrace
+|> Program.withDebugger
 |> Program.withHMR
 #endif
 |> Program.withReactNative "nightwatch"
