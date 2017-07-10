3b43dc507146d4e40d50e35b8bbc06bc07faa00a
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-07-10 12:15:04 +0200

    Compile to ES5

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var fable = require('rollup-plugin-fable')
+var babel = require('rollup-plugin-babel')
 
 function resolve(filePath) {
   return path.resolve(__dirname, filePath)
@@ -8,15 +9,29 @@ function resolve(filePath) {
 var isProduction = process.argv.indexOf("-w") === -1;
 console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
 
+var babelOptions = {
+  presets: [["es2015", { "modules": false }]],
+  plugins: ["external-helpers"]
+};
+
 export default {
   entry: resolve('src/Nightwatch.fsproj'),
   dest: resolve('out/Nightwatch.js'),
   format: 'es', // 'amd', 'cjs', 'es', 'iife', 'umd'
   //sourceMap: 'inline',
-  plugins: [fable({
-    extra: { failOnError: true },
-    define: isProduction ? ["PRODUCTION"] : ["DEBUG"]
-  })],
+  plugins: [
+    fable({
+      babel: babelOptions,
+      extra: { failOnError: true },
+      define: isProduction ? ["PRODUCTION"] : ["DEBUG"]
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      presets: babelOptions.presets,
+      plugins: babelOptions.plugins,
+      externalHelpers: true
+    })
+  ],
   external: [
     "buffer",
     "react",
