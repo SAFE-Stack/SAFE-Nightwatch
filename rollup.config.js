bd4050746bfb5a52cb58c259c3df24981cd4e5b8
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-07-11 10:46:59 +0200

    Revert "Compile to ES5 (#26)"
    This reverts commit bee41b10fc5d0308fc424c28f7c8cd8d7e0f84e4.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 var path = require('path')
 var fable = require('rollup-plugin-fable')
-var babel = require('rollup-plugin-babel')
 
 function resolve(filePath) {
   return path.resolve(__dirname, filePath)
@@ -9,29 +8,15 @@ function resolve(filePath) {
 var isProduction = process.argv.indexOf("-w") === -1;
 console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
 
-var babelOptions = {
-  presets: [["es2015", { "modules": false }]],
-  plugins: ["external-helpers"]
-};
-
 export default {
   entry: resolve('src/Nightwatch.fsproj'),
   dest: resolve('out/Nightwatch.js'),
   format: 'es', // 'amd', 'cjs', 'es', 'iife', 'umd'
   //sourceMap: 'inline',
-  plugins: [
-    fable({
-      babel: babelOptions,
-      extra: { failOnError: true },
-      define: isProduction ? ["PRODUCTION"] : ["DEBUG"]
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      presets: babelOptions.presets,
-      plugins: babelOptions.plugins,
-      externalHelpers: true
-    })
-  ],
+  plugins: [fable({
+    extra: { failOnError: true },
+    define: isProduction ? ["PRODUCTION"] : ["DEBUG"]
+  })],
   external: [
     "buffer",
     "react",
