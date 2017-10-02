2d7654a93cca98166a25168e125c6bd3f66272ab
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-10-02 08:17:34 +0200

    Use FlatList

--- src/Scenes/LocationList.fs
+++ src/Scenes/LocationList.fs
@@ -24,12 +24,12 @@ type Msg =
 | Error of exn
 
 type Model =
-  { RequestDataSource : ListViewDataSource<int * Model.LocationCheckRequest>
+  { Requests : (int * Model.LocationCheckRequest) []
     Status : Status }
 
 let init () =
     { Status = NotStarted
-      RequestDataSource = emptyDataSource() }, Cmd.ofMsg RefreshList
+      Requests = [||] }, Cmd.ofMsg RefreshList
 
 // Update
 let update msg model : Model*Cmd<Msg> =
@@ -44,7 +44,7 @@ let update msg model : Model*Cmd<Msg> =
 
     | NewLocationCheckRequests indexedRequests ->
         { model with
-            RequestDataSource = updateDataSource indexedRequests model.RequestDataSource
+            Requests = indexedRequests
             Status = Complete (sprintf "Locations: %d" indexedRequests.Length) }, Cmd.none
 
     | Error e ->
@@ -53,38 +53,33 @@ let update msg model : Model*Cmd<Msg> =
 
 // View
 let view (model:Model) (dispatch: Msg -> unit) =
-    let getListView() =
-          listView
-            model.RequestDataSource
-            [ ListViewProperties.RenderRow
-                (Func<_,_,_,_,_>(fun (pos,request: Model.LocationCheckRequest) b c d ->
-                    [view [
-                        ViewProperties.Style[
-                            FlexStyle.JustifyContent JustifyContent.Center
-                            FlexStyle.AlignItems ItemAlignment.Center
-                            FlexStyle.Flex 1.
-                            FlexStyle.FlexDirection FlexDirection.Row ]]
-                        [ text [ Styles.defaultText ] request.Name
-                          text [ Styles.defaultText ] request.Address
-                          (match request.Status with
-                           | None -> text [] ""
-                           | Some status ->
-                                let uri =
-                                    match status with
-                                    | Model.LocationStatus.Alarm text -> localImage "${entryDir}/../images/Alarm.png"
-                                    | _ -> localImage "${entryDir}/../images/Approve.png"
+    let renderItem (pos,request: Model.LocationCheckRequest) =
+        [view [
+            ViewProperties.Style[
+                FlexStyle.JustifyContent JustifyContent.Center
+                FlexStyle.AlignItems ItemAlignment.Center
+                FlexStyle.Flex 1.
+                FlexStyle.FlexDirection FlexDirection.Row ]]
+            [ text [ Styles.defaultText ] request.Name
+              text [ Styles.defaultText ] request.Address
+              (match request.Status with
+               | None -> text [] ""
+               | Some status ->
+                    let uri =
+                        match status with
+                        | Model.LocationStatus.Alarm text -> localImage "${entryDir}/../images/Alarm.png"
+                        | _ -> localImage "${entryDir}/../images/Approve.png"
 
-                                image
-                                    [ Source uri
-                                      ImageProperties.Style [
-                                            FlexStyle.Width 24.
-                                            FlexStyle.Height 24.
-                                            FlexStyle.AlignSelf Alignment.Center
-                                        ]
-                                    ])
-                    ]]
-                    |> touchableHighlight [OnPress (fun () -> dispatch (CheckNextLocation(pos,request)))]))
-            ]
+                    image
+                        [ Source uri
+                          ImageProperties.Style [
+                                FlexStyle.Width 24.
+                                FlexStyle.Height 24.
+                                FlexStyle.AlignSelf Alignment.Center
+                            ]
+                        ])
+        ]]
+        |> touchableHighlight [OnPress (fun () -> dispatch (CheckNextLocation(pos,request)))]
 
     view [ Styles.sceneBackground ]
         [ text [ Styles.titleText ] "Locations to check"
@@ -92,7 +87,12 @@ let view (model:Model) (dispatch: Msg -> unit) =
             (match model.Status with
              | Complete s -> s
              | _ -> "")
-          (if model.RequestDataSource.getRowCount() > 0 then getListView() else Styles.whitespace)
-
+          
+          
+          flatList model.Requests [
+              InitialNumToRender 20
+              KeyExtractor (Func<_,_,_>(fun (i,_) _ -> i.ToString()))
+              RenderItem (Func<_,_>(fun v -> renderItem v.item))
+          ]
           Styles.button "OK" (fun () -> dispatch GoBack)
         ]
