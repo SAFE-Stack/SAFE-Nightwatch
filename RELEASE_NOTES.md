6cb766551b7576a0d19db97383511c58211310b7
Tim Forkmann <tforkmann@gmail.com>
2020-12-08 22:50:44 +0100

    Bump to 1.0.1-alpha

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,4 @@
+#### 1.0.1-alpha - 08.12.2020
+* Update to Fable 3 and NET 5
 #### 1.0.0 - 17.10.2016
 * First release
--- src/ReleaseNotes.fs
+++ src/ReleaseNotes.fs
@@ -1,12 +1,14 @@
 module internal ReleaseNotes
 
-let AppVersion = "1.0.0"
+let AppVersion = "1.0.1-alpha"
 
-let IsPrerelease = false
+let IsPrerelease = true
 
-let AndroidVersionCode = 73
+let AndroidVersionCode = 75
 
 let Notes = """
+#### 1.0.1-alpha - 08.12.2020
+* Update to Fable 3 and NET 5
 #### 1.0.0 - 17.10.2016
 * First release
 """
